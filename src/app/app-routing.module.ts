28809af110be3a466ed169a9dad32ef58f687b97
sdugan01 <dugansea@gmail.com>
2020-04-23 09:09:55 -0400

    start of nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayerComponent } from './player/player.component';
+import { Songs } from 'src/app/mock-songs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'songs', component: PlayerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
